42b318f27f7d0dfaf6b0d0503619249d679721fd
Angus McLean <amclean@sti.systems>
2017-10-04 22:41:47 -0400

    working output redirection

--- A1/main.c
+++ A1/main.c
@@ -9,15 +9,36 @@
 #include <signal.h>
 #include <time.h>
 
-int commandNumber = 0;
+struct node {
+    int number; // the job number
+    int pid; // the process id of the a specific process
+    struct node *next; // when another process is called you add to the end of the linked list
+};
+
+struct jobParams {
+    char *args[20];
+	int bg;
+    int redirectFlag;
+    char *redirectTo;
+};
+
 struct node *head_job = NULL;
 struct node *current_job = NULL;
 
-//Initiliaze the args(input) to null once the command has been processed
-// this is to clear it to accept another command in the next while loop
-void initialize(char *args[]);
+void initialize(char *args[], struct jobParams *job) {
+	for (int i = 0; i < 20; i++) {
+		args[i] = NULL;
+	}
 
-int getcmd(char *line, char *args[], int *background) {
+    *(job->args) = *args;
+    job->bg = 0;
+    job->redirectFlag = 0;
+    job->redirectTo = NULL;
+
+    return;
+}
+
+int getcmd(char *line, char *args[], int *background, struct jobParams *nextJob) {
 	int i = 0;
 	char *token, *loc;
 
@@ -27,10 +48,19 @@ int getcmd(char *line, char *args[], int *background) {
 
 	// Check if background is specified..
 	if ((loc = index(line, '&')) != NULL) {
-		*background = 1;
+        nextJob->bg = 1;
+        *background = 1;
 		*loc = ' ';
-	} else
-		*background = 0;
+	} else {
+        nextJob->bg = 0;
+        *background = 0;
+    }
+
+    if (strstr(line, ">>") != NULL) {
+        nextJob->redirectFlag = O_APPEND;
+    } else if (strstr(line, ">") != NULL) {
+        nextJob->redirectFlag = O_TRUNC;
+    }
 
 	//Create a new line pointer to solve the problem of memory leaking created by strsep() and getline() when making line = NULL
 	char *lineCopy = line;
@@ -42,21 +72,14 @@ int getcmd(char *line, char *args[], int *background) {
 			args[i++] = token;
 	}
 
+    if(nextJob->redirectFlag != NULL) {
+        nextJob->redirectTo = args[i-1];
+        args[i-2] = NULL;
+        args[i-1] = NULL;
+    }
+
 	return i;
 }
-
-void initialize(char *args[]) {
-	for (int i = 0; i < 20; i++) {
-		args[i] = NULL;
-	}
-	return;
-}
-
-struct node {
-	int number; // the job number
-	int pid; // the process id of the a specific process
-	struct node *next; // when another process is called you add to the end of the linked list
-};
 
 /* Add a job to the list of jobs
  */
@@ -87,17 +110,22 @@ void addToJobList(int process_pid) {
 	}
 }
 
-int runJob(char *args[], int background) {
+int runJob(char *args[], int background, struct jobParams *nextJob) {
     pid_t  pid;     // this is just a type def for int really..
     pid = fork();
     if (pid == 0) {
         //Child process..
+        if(nextJob->redirectTo != NULL) {
+            printf("nextJob->redirectFlag %d\n", nextJob->redirectFlag);
+            close(1);
+            open(nextJob->redirectTo, O_WRONLY | O_CREAT | nextJob->redirectFlag, 0755);
+        }
         int w = rand() % 10;
-		sleep(w);
+        sleep(w);
         execvp(args[0], args);
     } else {
         int status;
-        if(background) {
+        if(nextJob->bg) {
             printf("Running %d in background\n", pid);
             addToJobList(pid);
         } else {
@@ -141,6 +169,7 @@ void printJob(struct node *pNode) {
 int main(void) {
 	char *args[20];
 	int bg;
+    struct jobParams nextJob = {*args, 0, 0, NULL};
 
 	char *user = getenv("USER");
 	if (user == NULL) user = "User";
@@ -153,7 +182,7 @@ int main(void) {
 
 
 	while (1) {
-		initialize(args);
+		initialize(args, &nextJob);
 		bg = 0;
 
 		int length = 0;
@@ -166,11 +195,13 @@ int main(void) {
 		if (length <= 0) { //if argument is empty
 			exit(-1);
 		}
-		int cnt  = getcmd(line, args, &bg);
+		int cnt  = getcmd(line, args, &bg, &nextJob);
 		if (!strcmp("ls", args[0])) { // returns 0 if they are equal , then we negate to make the if statment true
-			runJob(args, bg);
+			runJob(args, bg, &nextJob);
 		} else if (!strcmp("cat", args[0])) { // returns 0 if they are equal , then we negate to make the if statment true
-			runJob(args, bg);
+			runJob(args, bg, &nextJob);
+		} else if (!strcmp(args[0], "exit")) {
+			exit(-1);
 		} else if (!strcmp("cd", args[0])) {
 
 			int result = 0;
